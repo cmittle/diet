80f3d7751ba8ca691e1c87b0447fa91175977c72
cmittle <cory.mittleider@gmail.com>
2016-02-28 00:27:24 -0600

    add + / - expand / collaps to non-carbs formatting cleanup remove complicated switch statment in carbCtrl and replaced with simpler one.

--- app/carbCtrl.js
+++ app/carbCtrl.js
@@ -13,125 +13,90 @@ $scope.comboExpand = "+";
 $scope.sweetsExpand = "+";
 
 $scope.showGrainsSwitch = function () {  //turns grains on / off
-	$scope.showGrains = !$scope.showGrains;
-	switch ($scope.grainsExpand) {
-		case "+":
-			$scope.grainsExpand = "-";
-			break;	
-		case "-":
-			$scope.grainsExpand = "+";
-			break;
-	}
+    $scope.showGrains = !$scope.showGrains;
+    $scope.grainsExpand = ($scope.showGrains === true) ? '-' : '+';
 };
 
 $scope.showMilkSwitch = function () {  //turns millk on / off
-	$scope.showMilk = !$scope.showMilk;
-	switch ($scope.milkExpand) {
-		case "+":
-			$scope.milkExpand = "-";
-			break;	
-		case "-":
-			$scope.milkExpand = "+";
-			break;
-	}
+    $scope.showMilk = !$scope.showMilk;
+    $scope.milkExpand = ($scope.showMilk === true) ? '-' : '+';
 };
 
 $scope.showFruitsSwitch = function () {  //turns fruits on / off
-	$scope.showFruits = !$scope.showFruits;
-	switch ($scope.fruitsExpand) {
-		case "+":
-			$scope.fruitsExpand = "-";
-			break;	
-		case "-":
-			$scope.fruitsExpand = "+";
-			break;
-	}
+    $scope.showFruits = !$scope.showFruits;
+    $scope.fruitsExpand = ($scope.showFruits === true) ? '-' : '+';
 };
 
 $scope.showComboSwitch = function () {  //turns Combo on / off
-	$scope.showCombo = !$scope.showCombo;
-	switch ($scope.comboExpand) {
-		case "+":
-			$scope.comboExpand = "-";
-			break;	
-		case "-":
-			$scope.comboExpand = "+";
-			break;
-	}
+    $scope.showCombo = !$scope.showCombo;
+    $scope.comboExpand = ($scope.showCombo === true) ? '-' : '+';
 };
 
 $scope.showSweetsSwitch = function () {  //turns sweets on / off
-	$scope.showSweets = !$scope.showSweets;
-	switch ($scope.sweetsExpand) {
-		case "+":
-			$scope.sweetsExpand = "-";
-			break;	
-		case "-":
-			$scope.sweetsExpand = "+";
-			break;
-	}
+    $scope.showSweets = !$scope.showSweets;
+    $scope.sweetsExpand = ($scope.showSweets === true) ? '-' : '+';
 };
 	
 	
 $scope.grains = [
-	{name:"Bagel" , serving:"1 oz"},
-	{name:"Bread" , serving:"1 slice"},
-	{name:"English muffin", serving:"1/2 muffin"},
-	{name:"Cereal cooked" , serving:"1/2 cup"},
-	{name:"Cereal ready to eat, unsweetened" , serving:"3/4 cup"},
-	{name:"Starch veg, Corn, peas, mashed white or sweet, yam" , serving:"1/2 cup"},
-	{name:"Starch veg, Potato, baked or boiled" , serving:"1 small (3oz)"},
-	{name:"Starch veg, Squash (acorn or butternut), cooked" , serving:"1 cup"},
-	{name:"Hamburger or hot dog bun" , serving:"1/2 bun"},
-	{name:"Beans (pinto, lima, kidney, garbanzo)" , serving:"1/2 cup"},
-	{name:"Baked beans" , serving:"1/3 cup"},
-	{name:"Pasta or rice, cooked" , serving:"1/3 cup"},
-	{name:"Pancakes/waffles" , serving:"1 4-inch"},
-	{name:"Tortilla" , serving:"1 6-inch"}
+    {name:"Bagel" , serving:"1 oz"},
+    {name:"Bread" , serving:"1 slice"},
+    {name:"English muffin", serving:"1/2 muffin"},
+    {name:"Cereal cooked" , serving:"1/2 cup"},
+    {name:"Cereal ready to eat, unsweetened" , serving:"3/4 cup"},
+    {name:"Starch veg, Corn, peas, mashed white or sweet, yam" , serving:"1/2 cup"},
+    {name:"Starch veg, Potato, baked or boiled" , serving:"1 small (3oz)"},
+    {name:"Starch veg, Squash (acorn or butternut), cooked" , serving:"1 cup"},
+    {name:"Hamburger or hot dog bun" , serving:"1/2 bun"},
+    {name:"Beans (pinto, lima, kidney, garbanzo)" , serving:"1/2 cup"},
+    {name:"Baked beans" , serving:"1/3 cup"},
+    {name:"Pasta or rice, cooked" , serving:"1/3 cup"},
+    {name:"Pancakes/waffles" , serving:"1 4-inch"},
+    {name:"Tortilla" , serving:"1 6-inch"}
 ];
 
 $scope.milk = [
-	{name:"Milk, skim, 1%, 2%, whole or soy", serving:"1 cup (8 oz)"},
-	{name:"Chocolate milk", serving:"1/2 cup"},
-	{name:"Yogurt - lite or artificial sweetener", serving:"6-8 oz"},
-	{name:"Yogurt - with sugar", serving:"4-6 oz"},
-	{name:"Ice cream, regular or lite", serving:"1/2 cup"},
-	{name:"Pudding, sugar-free", serving:"1/2 cup"}
-	];
+    {name:"Milk, skim, 1%, 2%, whole or soy", serving:"1 cup (8 oz)"},
+    {name:"Chocolate milk", serving:"1/2 cup"},
+    {name:"Yogurt - lite or artificial sweetener", serving:"6-8 oz"},
+    {name:"Yogurt - with sugar", serving:"4-6 oz"},
+    {name:"Ice cream, regular or lite", serving:"1/2 cup"},
+    {name:"Pudding, sugar-free", serving:"1/2 cup"}
+];
 	
 $scope.fruits = [
-	{name:"Apple, orange or pear", serving:"1 small"},
-	{name:"Cherries or grapes", serving:"12-15"},
-	{name:"Fruit juice (100% real juice)", serving:"1/3 - 1/2 cup"},
-	{name:"Melon or berries", serving:"1 cup"},
-	{name:"Canned fruit, light", serving:"1/2 cup"},
-	{name:"Grapefruit", serving:"1/2 medium"},
-	{name:"Banana", serving:"4 inch"},
-	{name:"Raisins or prunes", serving:"2 Tbsp"},
-	{name:"Dried fruit", serving:"1/4 cup"},
-	{name:"V8 100% vegetable juice", serving:"1 1/2 cups (12oz)"}
-	];
+    {name:"Apple, orange or pear", serving:"1 small"},
+    {name:"Cherries or grapes", serving:"12-15"},
+    {name:"Fruit juice (100% real juice)", serving:"1/3 - 1/2 cup"},
+    {name:"Melon or berries", serving:"1 cup"},
+    {name:"Canned fruit, light", serving:"1/2 cup"},
+    {name:"Grapefruit", serving:"1/2 medium"},
+    {name:"Banana", serving:"4 inch"},
+    {name:"Raisins or prunes", serving:"2 Tbsp"},
+    {name:"Dried fruit", serving:"1/4 cup"},
+    {name:"V8 100% vegetable juice", serving:"1 1/2 cups (12oz)"}
+];
 
 
 $scope.combo = [
-	{name:"Chili, casserole or hot dish", serving:"1/2 cup"},
-	{name:"Potato, pasta or fruit salad", serving:"1/2 cup"},
-	{name:"Pizza, thin-crust, medium", serving:"1 slice (1/8 pizza)"},
-	{name:"Soup (bean, noodle or vegetable)", serving:"1 cup"},
-	{name:"Sub sandwich", serving:"2 inches"},
-	{name:"Lasagna", serving:'2" x 3"'}
+    {name:"Chili, casserole or hot dish", serving:"1/2 cup"},
+    {name:"Potato, pasta or fruit salad", serving:"1/2 cup"},
+    {name:"Pizza, thin-crust, medium", serving:"1 slice (1/8 pizza)"},
+    {name:"Soup (bean, noodle or vegetable)", serving:"1 cup"},
+    {name:"Sub sandwich", serving:"2 inches"},
+    {name:"Lasagna", serving:'2" x 3"'}
 ];
 
 
 
 $scope.sweets = [
-	{name:'Cookie, 3" across', serving:"1 cookie"},
-	{name:"Cake, bar or brownie, unfrosted", serving:'2" square'},
-	{name:"Saltines", serving:"6 crackers"},
-	{name:"Popcorn, poped, unflavored", serving:"3 cups"},
-	{name:"Chips, French fries", serving:"10-12"},
-	{name:"Regular syrup, jam, sugar, honey", serving:"1 Tbsp"},
-	{name:"Ketcup", serving:"1/4 cup"}
+    {name:'Cookie, 3" across', serving:"1 cookie"},
+    {name:"Cake, bar or brownie, unfrosted", serving:'2" square'},
+    {name:"Saltines", serving:"6 crackers"},
+    {name:"Popcorn, poped, unflavored", serving:"3 cups"},
+    {name:"Chips, French fries", serving:"10-12"},
+    {name:"Regular syrup, jam, sugar, honey", serving:"1 Tbsp"},
+    {name:"Ketcup", serving:"1/4 cup"}
 ];
 
 
--- app/nonCarbCtrl.js
+++ app/nonCarbCtrl.js
@@ -6,98 +6,108 @@ $scope.showFat = false;
 $scope.showFree = false;
 $scope.showVegetables = false;
 
+$scope.meatExpand = "+";
+$scope.proteinExpand = "+";
+$scope.fatsExpand = "+";
+$scope.freeExpand = "+";
+$scope.vegetablesExpand = "+";
+
 $scope.showMeatSwitch = function () {  //turns Meat on / off
-	$scope.showMeat = !$scope.showMeat;
+    $scope.showMeat = !$scope.showMeat;
+    $scope.meatExpand = ($scope.showMeat === true) ? '-' : '+';
 };
 
 $scope.showProteinSwitch = function () {  //turns millk on / off
-	$scope.showProtein = !$scope.showProtein;
+    $scope.showProtein = !$scope.showProtein;
+    $scope.proteinExpand = ($scope.showProtein === true) ? '-' : '+';
 };
 
 $scope.showFatSwitch = function () {  //turns Fat on / off
-	$scope.showFat = !$scope.showFat;
+    $scope.showFat = !$scope.showFat;
+    $scope.fatsExpand = ($scope.showFat === true) ? '-' : '+';
 };
 
 $scope.showFreeSwitch = function () {  //turns Free on / off
-	$scope.showFree = !$scope.showFree;
+    $scope.showFree = !$scope.showFree;
+    $scope.freeExpand = ($scope.showFree === true) ? '-' : '+';
 };
 
 $scope.showVegetablesSwitch = function () {  //turns Vegetables on / off
-	$scope.showVegetables = !$scope.showVegetables;
+    $scope.showVegetables = !$scope.showVegetables;
+    $scope.vegetablesExpand = ($scope.showVegetables === true) ? '-' : '+';
 };
 	
 	
 $scope.Meat = [
-	{name:"Beef, ground, lean or extra lean" , serving:""},
-	{name:"Chicken, turckey" , serving:""},
-	{name:"Fish or seafood (1/4 cup = 1 oz", serving:""},
-	{name:"Ham" , serving:""},
-	{name:"Pork" , serving:""},
-	{name:"Lamb" , serving:""},
-	{name:"Veal" , serving:""},
-	{name:"Lunch meat" , serving:"-use less often"},
-	{name:"Hot Dog" , serving:"-use less often"},
-	{name:"Sausage" , serving:"-use less often"}
+    {name:"Beef, ground, lean or extra lean" , serving:""},
+    {name:"Chicken, turckey" , serving:""},
+    {name:"Fish or seafood (1/4 cup = 1 oz", serving:""},
+    {name:"Ham" , serving:""},
+    {name:"Pork" , serving:""},
+    {name:"Lamb" , serving:""},
+    {name:"Veal" , serving:""},
+    {name:"Lunch meat" , serving:"-use less often"},
+    {name:"Hot Dog" , serving:"-use less often"},
+    {name:"Sausage" , serving:"-use less often"}
 ];
 
 $scope.Protein = [
-	{name:"Cheese (1 oz of cheese = 1 oz of meat)", serving:""},
-	{name:"Cottage cheese (1/4 cup = 1 oz of meat", serving:""},
-	{name:"Egg (1 egg = 1 oz of meat", serving:""},
-	{name:"Peanut butter (2 Tbsp = 1 oz", serving:""}
-	];
+    {name:"Cheese (1 oz of cheese = 1 oz of meat)", serving:""},
+    {name:"Cottage cheese (1/4 cup = 1 oz of meat", serving:""},
+    {name:"Egg (1 egg = 1 oz of meat", serving:""},
+    {name:"Peanut butter (2 Tbsp = 1 oz", serving:""}
+];
 	
 $scope.Fat = [
-	{name:"Tub margarine, mayonnaise, oil (canola, olive)", serving:"1 tsp"},
-	{name:"Nuts, seeds", serving:"1 Tbsp"},
-	{name:"Salad dressing - Regular", serving:"1 Tbsp"},
-	{name:"Salad dressing - Reduced-fat", serving:"2 Tbsp"},
-	{name:"Avocado", serving:"2 Tbsp"},
-	{name:"Bacon", serving:"1 Slice"},
-	{name:"Butter", serving:"t tsp"},
-	{name:"Cream cheese", serving:"1 Tbsp"},
-	{name:"Gravy, sour cream, plain coffee creamer", serving:"2 Tbsp"},
-	{name:"Olives, medium or large", serving:"10"}
-	];
+    {name:"Tub margarine, mayonnaise, oil (canola, olive)", serving:"1 tsp"},
+    {name:"Nuts, seeds", serving:"1 Tbsp"},
+    {name:"Salad dressing - Regular", serving:"1 Tbsp"},
+    {name:"Salad dressing - Reduced-fat", serving:"2 Tbsp"},
+    {name:"Avocado", serving:"2 Tbsp"},
+    {name:"Bacon", serving:"1 Slice"},
+    {name:"Butter", serving:"t tsp"},
+    {name:"Cream cheese", serving:"1 Tbsp"},
+    {name:"Gravy, sour cream, plain coffee creamer", serving:"2 Tbsp"},
+    {name:"Olives, medium or large", serving:"10"}
+];
 
 $scope.Free = [
-	{name:"Club soda", serving:""},
-	{name:"Coffee", serving:""},
-	{name:"Jello, sugar-free (no added fruit)", serving:""},
-	{name:"Seasonings", serving:""},
-	{name:"Soft drinks, diet", serving:""},
-	{name:"Sugar substitutes", serving:""},
-	{name:"Tea, hot or iced, unsweetened", serving:""},
-	{name:"Water, plain or unsweetened", serving:""},
-	{name:"Salsa", serving:""},
-	{name:"Ketchup", serving:""},
-	{name:"Mustard", serving:""}
+    {name:"Club soda", serving:""},
+    {name:"Coffee", serving:""},
+    {name:"Jello, sugar-free (no added fruit)", serving:""},
+    {name:"Seasonings", serving:""},
+    {name:"Soft drinks, diet", serving:""},
+    {name:"Sugar substitutes", serving:""},
+    {name:"Tea, hot or iced, unsweetened", serving:""},
+    {name:"Water, plain or unsweetened", serving:""},
+    {name:"Salsa", serving:""},
+    {name:"Ketchup", serving:""},
+    {name:"Mustard", serving:""}
 ];
 
 $scope.Vegetables = [
-	{name:"Asparagus", serving:""},
-	{name:"Beans (green, wax, Italian)", serving:""},
-	{name:"Beets", serving:""},
-	{name:"Broccoli", serving:""},
-	{name:"Brussel sprouts", serving:""},
-	{name:"Cabbage", serving:""},
-	{name:"Carrots", serving:""},
-	{name:"Cauliflower", serving:""},
-	{name:"Celery", serving:""},
-	{name:"Cucumbers", serving:""},
-	{name:"Eggplant", serving:""},
-	{name:"Lettuce", serving:""},
-	{name:"Mushrooms", serving:""},
-	{name:"Onions", serving:""},
-	{name:"Pea pods", serving:""},
-	{name:"Peppers", serving:""},
-	{name:"Radishes", serving:""},
-	{name:"Spinach", serving:""},
-	{name:"Tomatoes", serving:""},
-	{name:"Turnips", serving:""},
-	{name:"Water chestnuts", serving:""},
-	{name:"Zucchini", serving:""}
-	
+    {name:"Asparagus", serving:""},
+    {name:"Beans (green, wax, Italian)", serving:""},
+    {name:"Beets", serving:""},
+    {name:"Broccoli", serving:""},
+    {name:"Brussel sprouts", serving:""},
+    {name:"Cabbage", serving:""},
+    {name:"Carrots", serving:""},
+    {name:"Cauliflower", serving:""},
+    {name:"Celery", serving:""},
+    {name:"Cucumbers", serving:""},
+    {name:"Eggplant", serving:""},
+    {name:"Lettuce", serving:""},
+    {name:"Mushrooms", serving:""},
+    {name:"Onions", serving:""},
+    {name:"Pea pods", serving:""},
+    {name:"Peppers", serving:""},
+    {name:"Radishes", serving:""},
+    {name:"Spinach", serving:""},
+    {name:"Tomatoes", serving:""},
+    {name:"Turnips", serving:""},
+    {name:"Water chestnuts", serving:""},
+    {name:"Zucchini", serving:""}
 ];
 
 
